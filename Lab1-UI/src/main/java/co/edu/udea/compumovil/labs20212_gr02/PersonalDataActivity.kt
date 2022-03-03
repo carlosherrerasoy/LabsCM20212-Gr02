361f63e6498080224439871a63de5a8188da7d05
Arbey <arbpoe@gmail.com>
2022-03-02 22:36:31 -0500

    Adicionar funcionalidad del DatePicker

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/labs20212_gr02/PersonalDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/labs20212_gr02/PersonalDataActivity.kt
@@ -1,21 +1,26 @@
 package co.edu.udea.compumovil.labs20212_gr02
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.RadioButton
-import android.widget.Spinner
+import android.view.View
+import android.widget.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class PersonalDataActivity : AppCompatActivity() {
+
+
+    val calendar = Calendar.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_data)
         nextClickButtonListener()
+        fechaNacimientoEditTextListener()
     }
-
 
     private fun tryToGenerateLogsAndOpenActivity() {
         if (areFieldsRequiredFilled()) {
@@ -46,6 +51,10 @@ class PersonalDataActivity : AppCompatActivity() {
             valorSexo
         )
 
+        val fechaNacimientoTextView = findViewById<TextView>(R.id.fechaNacimientoTextView)
+        Log.i(getString(R.string.fechaNacimientoLabelText), fechaNacimientoTextView.text.toString())
+
+
         val escolaridadSpinner = findViewById<Spinner>(R.id.escolaridadSpinner)
         val itemPosition = escolaridadSpinner.selectedItemPosition
 
@@ -65,6 +74,33 @@ class PersonalDataActivity : AppCompatActivity() {
         }
     }
 
+    private fun fechaNacimientoEditTextListener() {
+        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(view: DatePicker, year: Int, monthOfYear: Int,
+                                   dayOfMonth: Int) {
+                calendar.set(Calendar.YEAR, year)
+                calendar.set(Calendar.MONTH, monthOfYear)
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateDateInView(getFechaNacimientoSeleccionado())
+            }
+        }
+
+
+        val cambiarFechaNacimientoBoton = findViewById<Button>(R.id.cambiarFechaBoton)
+
+        cambiarFechaNacimientoBoton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                DatePickerDialog(this@PersonalDataActivity,
+                    dateSetListener,
+                    // set DatePickerDialog to point to today's date when it loads up
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)).show()
+            }
+
+        })
+    }
+
     private fun openContactActivity() {
         val contactActivity = Intent(this, ContactDataActivity::class.java)
         startActivity(contactActivity)
@@ -73,13 +109,13 @@ class PersonalDataActivity : AppCompatActivity() {
     private fun areFieldsRequiredFilled(): Boolean {
         val nombresEditTextId = R.id.nombreEditText
         val apellidosEditTextId = R.id.apellidosEditText
-        val nacimientoEditTextId = R.id.fechaNacimientoEditDate
+        val nacimientoTextViewId = R.id.fechaNacimientoTextView
 
 
         val validatedNombres: Boolean = validateIsEmptyEditText(nombresEditTextId)
         val validatedApellidosEditTextId: Boolean = validateIsEmptyEditText(apellidosEditTextId)
-        val validatedNacimientoEditTextId: Boolean = validateIsEmptyEditText(nacimientoEditTextId)
-        if (validatedNombres && validatedApellidosEditTextId && validatedNacimientoEditTextId) {
+        val validatedNacimientoTextViewId: Boolean = validateFechaNacimiento(nacimientoTextViewId)
+        if (validatedNombres && validatedApellidosEditTextId && validatedNacimientoTextViewId) {
             return true;
         }
         return false
@@ -93,5 +129,42 @@ class PersonalDataActivity : AppCompatActivity() {
             return false
         }
         return true
+    }
+    private fun validateFechaNacimiento(fieldId: Int): Boolean {
+        val textView = findViewById<TextView>(fieldId)
+        if (textView.text.toString().equals(getString(R.string.fechaNacimientoLabelText))) {
+            textView.error = getString(R.string.required)
+            return false
+        }
+        return true
+    }
+
+    private fun updateDateInView(fechaNacimiento: String) {
+        val fechaNacimientoView = findViewById<TextView>(R.id.fechaNacimientoTextView)
+        fechaNacimientoView!!.text = fechaNacimiento
+    }
+
+    private fun getFechaNacimientoSeleccionado (): String {
+        val myFormat = "dd/MM/yyyy"
+        val sdf = SimpleDateFormat(myFormat)
+        return sdf.format(calendar.getTime())
+    }
+
+    private fun getFechaNacimientoTextView(): String {
+        val fechaNacimientoView = findViewById<TextView>(R.id.fechaNacimientoTextView)
+        return fechaNacimientoView.text.toString()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString("fecha_nacimiento", getFechaNacimientoTextView())
+
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        if (savedInstanceState.containsKey("fecha_nacimiento"))
+            updateDateInView(savedInstanceState.getString("fecha_nacimiento").toString())
+
+        super.onRestoreInstanceState(savedInstanceState)
     }
 }
